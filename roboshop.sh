ab82ec0060155f58406d68032b131751d3fabc78
sridhar <mshridhar225@gmail.com>
2025-10-04 07:43:48 +0530

    colors

--- roboshop.sh
+++ roboshop.sh
@@ -3,16 +3,16 @@
 
 AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-0a95ef1237f82de1c"
-INSTANCES=("MONGODB" "REDIS" "MYSQL" "RABBITMQ" "CATALOGUE" "USER" "CART" "PAYMENT" "SHIPPING" "DISPATCH")
+INSTANCES=("MONGODB" "REDIS" "MYSQL" "RABBITMQ" "CATALOGUE" "USER" "CART" "PAYMENT" "SHIPPING" "DISPATCH" "FRONTEND")
 ZONE_ID="Z04210263NS79GNN5JP59"
 DOMAIN_NAME="sridharmeka.site"
 
-for INSTANCES in ${INSTANCES[@]}
+for instances in ${INSTANCES[@]}
 do 
     INSTANCES_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-0a95ef1237f82de1c --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
     --query "Instances[0].InstanceId" --output text)
 
-if  [ "$instance" != "frontend" ]
+if  [ $instance != "frontend" ]
 then
     ip=$(aws ec2 describe-instances --instance-ids $INSTANCES_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
 else

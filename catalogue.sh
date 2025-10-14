4a4f1f5f0ba6a55ed177a93adc9792af8884370d
sridhar <mshridhar225@gmail.com>
2025-10-14 14:11:59 +0530

    shell-roboshop

---
+++ catalogue.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/shellscript-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+SCRIPT_DIR=$PWD
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
+    exit 1 #give other than 0 upto 127
+else
+    echo "You are running with root access" | tee -a $LOG_FILE
+fi
+
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+        echo -e " $2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
+    else
+        echo -e " $2 is ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
+
+dnf module disable nodejs -y &>>$LOG_FILE
+VALIDATE $? "Disabling default NodeJS"
+
+dnf module enable nodejs:20 -y &>>$LOG_FILE
+VALIDATE $? "Enabling NodeJS:20"
+
+dnf install nodejs -y &>>$LOG_FILE
+VALIDATE $? "Installing NodeJS:20"
+
+useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+VALIDATE $? "Creating roboshop system user"
+
+mkdir /app
+VALIDATE $? "Creating /app directory"
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip 
+VALIDATE $? "Downloading catalogue"
+
+cd /app 
+unzip /tmp/catalogue.zip &>>$LOG_FILE
+VALIDATE $? "unziping catalogue"
+
+npm install &>>$LOG_FILE
+VALIDATE $? "Installing Dependencies"
+
+cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service
+VALIDATE $? "Copying catalogue service"
+
+systemctl daemon-reload &>>$LOG_FILE
+systemctl enable catalogue 
+systemctl start catalogue
+VALIDATE $? "starting catalogue"
+
+cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongodb.repo
+dnf install mongodb-mongosh -y &>>$LOG_FILE
+VALIDATE $? "Installing MongoDB client"
+
+mongosh --host mongodb.sridharmeka.site </app/schema/catalogue.js &>>$LOG_FILE

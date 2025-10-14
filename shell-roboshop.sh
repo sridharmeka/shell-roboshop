04942bff5f6bbe7a727697aa3a367525dab6f259
sridhar <mshridhar225@gmail.com>
2025-10-14 16:22:01 +0530

    shell-roboshop

--- shell-roboshop.sh
+++ shell-roboshop.sh
@@ -6,18 +6,18 @@ INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipp
 ZONE_ID="Z04210263NS79GNN5JP59" 
 DOMAIN_NAME="sridharmeka.site"
 
-for instance in ${INSTANCES[@]}
+for instance in $@
 do 
   INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-0fd3ac047a50d777a --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ]
     then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].
         Instances[0].PrivateIpAddress" --output text)
-        
+        RECORD_NAME=$instance.$DOMAIN_NAME
     else
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].
         Instances[0].PublicIpAddress" --output text)
-        
+        RECORD_NAME="$DOMAIN_NAME"
     fi
     echo "$instance IP address: $IP"
 
@@ -29,7 +29,7 @@ do
         ,"Changes": [{
         "Action"              : "UPSERT"
         ,"ResourceRecordSet"  : {
-            "Name"              : "'$instance'.'$DOMAIN_NAME'"
+            "Name"              : "'$RECORD_NAME'"
             ,"Type"             : "A"
             ,"TTL"              : 1
             ,"ResourceRecords"  : [{

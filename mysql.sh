ad9cc5d794c5eb9db08fc2a17edc7d65116845fe
sridhar <mshridhar225@gmail.com>
2025-10-25 13:34:33 +0530

    shell-roboshop

--- mysql.sh
+++ mysql.sh
@@ -9,7 +9,6 @@ N="\e[0m"
 LOGS_FOLDER="/var/log/shell-roboshop"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
-START_TIME=$(date +%s)
 SCRIPT_DIR=$PWD
 
 mkdir -p $LOGS_FOLDER
